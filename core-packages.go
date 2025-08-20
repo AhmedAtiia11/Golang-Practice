d261d774e0386ee4ca9826ef85edf12d872dffa1
Your Name <you@example.com>
2025-08-20 22:40:56 +0300

    update core package

--- core-packages.go
+++ core-packages.go
@@ -6,9 +6,13 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -47,53 +51,79 @@ func main() {
 	pass = "KodeKloud"
 	fmt.Println(encodingwithmd5(pass))
 	fmt.Println("################ file handling Package in Go ###################")
-	path := filepath.Join("D:/", "atiia_practice", "mail.md")
-	fmt.Println(path)
-	fmt.Println(filepath.Dir(path))
-	fmt.Println(filepath.Base(path))
-	fmt.Println(filepath.Ext(path))
-	fmt.Println(filepath.IsAbs(path))
-	fmt.Println("################ file Info Package in Go ###################")
-	fileInfo, err4 := os.Stat(path)
-	if err4 != nil {
-		fmt.Println(err4)
-	}
-	fmt.Println(fileInfo.Name())
-	fmt.Println(fileInfo.Size())
-	fmt.Println(fileInfo.Mode())
-	fmt.Println(fileInfo.IsDir())
-	fmt.Println("################ Read file in Go ###################")
-	data, err5 := os.ReadFile(path)
-	if err5 != nil {
-		fmt.Println(err5)
-	}
-	fmt.Println(string(data))
+	// path := filepath.Join("D:/", "atiia_practice", "mail.md")
+	// fmt.Println(path)
+	// fmt.Println(filepath.Dir(path))
+	// fmt.Println(filepath.Base(path))
+	// fmt.Println(filepath.Ext(path))
+	// fmt.Println(filepath.IsAbs(path))
+	// fmt.Println("################ file Info Package in Go ###################")
+	// fileInfo, err4 := os.Stat(path)
+	// if err4 != nil {
+	// 	fmt.Println(err4)
+	// }
+	// fmt.Println(fileInfo.Name())
+	// fmt.Println(fileInfo.Size())
+	// fmt.Println(fileInfo.Mode())
+	// fmt.Println(fileInfo.IsDir())
+	// fmt.Println("################ Read file in Go ###################")
+	// data, err5 := os.ReadFile(path)
+	// if err5 != nil {
+	// 	fmt.Println(err5)
+	// }
+	// fmt.Println(string(data))
 
-	file, err6 := os.Open(path)
-	if err6 != nil {
-		fmt.Println(err6)
-	} else {
-		buffer := make([]byte, 5)
-		for {
-			n, err6 := file.Read(buffer)
-			if err6 != nil {
-				fmt.Println(err6)
-				break
-			}
-			fmt.Println(string(buffer[:n]))
-		}
-	}
+	// file, err6 := os.Open(path)
+	// if err6 != nil {
+	// 	fmt.Println(err6)
+	// } else {
+	// 	buffer := make([]byte, 5)
+	// 	for {
+	// 		n, err6 := file.Read(buffer)
+	// 		if err6 != nil {
+	// 			fmt.Println(err6)
+	// 			break
+	// 		}
+	// 		fmt.Println(string(buffer[:n]))
+	// 	}
+	// }
 	fmt.Println("################ Write file in Go ###################")
-	path2 := filepath.Join("D:/", "atiia_practice", "test.txt")
-	file, err7 := os.OpenFile(path2, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0660)
-	if err7 != nil {
-		fmt.Println(err7)
-	}
-	_, err8 := file.WriteString("Testing")
-	if err8 != nil {
-		fmt.Println(err8)
-	}
+	// path2 := filepath.Join("D:/", "atiia_practice", "test.txt")
+	// file, err7 := os.OpenFile(path2, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0660)
+	// if err7 != nil {
+	// 	fmt.Println(err7)
+	// }
+	// _, err8 := file.WriteString("Testing")
+	// if err8 != nil {
+	// 	fmt.Println(err8)
+	// }
 	fmt.Println("################ Logging in Go ###################")
+	log.Println("Hello")
+	path3 := filepath.Join("/media", "aattia", "DATA", "projects", "Go-practice", "test.md")
+	fmt.Println(path3)
+	file, err9 := os.OpenFile(path3, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err9 != nil {
+		fmt.Println(err9)
+	}
+	log.SetOutput(file)
+	log.Println("write log to a file")
+	log.Println("write log2 to a file")
+
+	logrus.Println("Log using Logrus")
+	logrus.SetOutput(file)
+	// logrus.Fatal("This is fatal log")
+	// logrus.Panic("This is panic log")
+	logrus.Error("This is error log")
+	// logrus.Warn("This is Warn log")
+	// logrus.Info("This is Info log")
+	fmt.Println("################ Sorting in Go ###################")
+	nums := []int{1, 7, 5}
+	sort.Ints(nums)
+	fmt.Println(nums)
+	letters := []string{"b", "s", "a"}
+	sort.Strings(letters)
+	fmt.Println(letters)
+	fmt.Println("################ Testing in Go ###################")
 
 }
 
---
+++ test.md
@@ -0,0 +1,26 @@
+2025/08/20 00:08:59 write log to a file
+2025/08/20 00:09:54 write log to a file
+2025/08/20 00:09:54 write log3 to a file
+2025/08/20 00:13:49 write log to a file
+2025/08/20 00:13:49 write log2 to a file
+2025/08/20 00:16:42 write log to a file
+2025/08/20 00:16:42 write log2 to a file
+[31mFATA[0m[0000] This is fatal log                            
+2025/08/20 00:17:28 write log to a file
+2025/08/20 00:17:28 write log2 to a file
+[31mPANI[0m[0000] This is panic log                            
+2025/08/20 00:17:59 write log to a file
+2025/08/20 00:17:59 write log2 to a file
+[31mERRO[0m[0000] This is error log                            
+2025/08/20 00:18:20 write log to a file
+2025/08/20 00:18:20 write log2 to a file
+[31mPANI[0m[0000] This is panic log                            
+2025/08/20 00:25:07 write log to a file
+2025/08/20 00:25:07 write log2 to a file
+[31mERRO[0m[0000] This is error log                            
+2025/08/20 00:25:25 write log to a file
+2025/08/20 00:25:25 write log2 to a file
+[31mERRO[0m[0000] This is error log                            
+2025/08/20 00:29:21 write log to a file
+2025/08/20 00:29:21 write log2 to a file
+[31mERRO[0m[0000] This is error log                            
